b66b22ebdfd1afefd6a52d439e04f2966f30b244
Yotau Hong <crazymousethief@outlook.com>
2018-04-02 00:12:40 +0800

    ✨ Add a simple Monaco editor

--- Compiler-Principle/index.js
+++ Compiler-Principle/index.js
@@ -1,1 +1,25 @@
-console.log('Compiler Principle')
+require.config({ paths: { vs: '../node_modules/monaco-editor/min/vs' } })
+
+require(['vs/editor/editor.main'], function() {
+  const editor = monaco.editor.create(document.getElementById('editor'), {
+    value: `#include <stdio.h>
+  int main() {
+  int a = 1;
+  return 0;
+}`,
+    language: 'c',
+  })
+
+  const output = monaco.editor.create(document.getElementById('result'), {
+    value: `#include <stdio.h>
+  int main() {
+  int a = 1;
+  return 0;
+}`,
+    language: 'c',
+  })
+
+  editor.getModel().onDidChangeContent(function() {
+    output.setValue(editor.getModel().getValue())
+  })
+})
