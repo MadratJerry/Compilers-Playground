2a5c78020c34c641e37d4d03b3ebf1f4f92df04b
Yotau Hong <crazymousethief@outlook.com>
2018-04-27 01:49:25 +0800

    ✨ Implement Lexical Definition

--- src/tokenizer/re-tokenizer.ts
+++ src/tokenizer/re-tokenizer.ts
@@ -1,51 +1,50 @@
 import Token from './token'
-class SyntaxDefinition {
-  private re: RegExp
-  name: string
-  groups: number
 
-  constructor(name: string, regexp: RegExp) {
-    this.re = regexp
-    this.name = name
-    this.groups = SyntaxDefinition.getGroups(this.re.source)
+class LexicalDefinition {
+  regex: RegExp
+  captureMap: Map<number, string> = new Map()
+  constructor(def: object) {
+    Object.entries(def).reduce((c, e) => {
+      const [name, regex] = e
+      const groups = LexicalDefinition.getGroups(regex.source)
+      for (let i = c; i <= c + groups; i++) this.captureMap.set(i, name)
+      return c + groups + 1
+    }, 1)
+    this.regex = new RegExp(
+      Object.values(def)
+        .map(r => `(${r.source})`)
+        .join('|'),
+      'g',
+    )
   }
 
+  // How many capturing groups in regular expression
   static getGroups(s: string): number {
-    const stack: Array<string> = []
     let ignore = false,
       pass = false,
-      top,
       count = 0
+
+    // Because each regular expression is correct, so just count the left bracket
     for (const i of s) {
       if (ignore) ignore = false
       else if (i === ']') pass = false
       else if (pass) continue
       else if (i === '[') pass = true
       else if (i === '\\') ignore = true
-      else if (i === '(') {
-        stack.push(i)
-        top = i
-      } else if (i === ')' && top === '(') {
-        stack.pop()
-        top = stack[stack.length - 1]
-        count++
-      }
+      else if (i === '(') count++
     }
-    return count
-  }
 
-  static merge(sds: Array<SyntaxDefinition>): RegExp {
-    return new RegExp(sds.map(s => `(${s.re.source})`).join('|'), 'g')
+    // Remove the non-capturing group
+    return count - s.split('(?:').length + 1
   }
 }
 
-const sds: Array<SyntaxDefinition> = [
-  new SyntaxDefinition('Identifiers', /([a-zA-Z_\$][\w\$]*)(\s*)(:?)/),
-  new SyntaxDefinition('Brackets', /[{}()\[\]]/),
-  new SyntaxDefinition('Delimiter', /[;,.]/),
-]
-
-const re = SyntaxDefinition.merge(sds)
+const ld = new LexicalDefinition({
+  Identifiers: /([a-zA-Z_\$][\w\$]*)(\s*)(:?)/,
+  Brackets: /[{}()\[\]]/,
+  Delimiter: /[;,.]/,
+  Symbols: /[~!@#%\^&*-+=|\\:`<>.?\/]+/,
+})
 
 class RETokenizer {
   text: string
@@ -53,11 +52,18 @@ class RETokenizer {
   constructor(text: string) {
     this.text = text
     this.match()
+    console.log(this.tokens)
   }
 
   match() {
-    for (let m = re.exec(this.text); m != null; m = re.exec(this.text)) {
-      console.log(m)
+    for (let m = ld.regex.exec(this.text); m != null; m = ld.regex.exec(this.text)) {
+      this.tokens.push(
+        new Token(
+          m[0],
+          { start: { line: 1, column: 0 }, end: { line: 1, column: 0 } },
+          ld.captureMap.get(m.lastIndexOf(m[0])),
+        ),
+      )
     }
   }
 }
