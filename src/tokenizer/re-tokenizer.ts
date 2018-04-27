0cb379f6a727087cab33eab3a99bea8c1582626f
Yotau Hong <crazymousethief@outlook.com>
2018-04-27 23:46:59 +0800

    ✨ Rewrite LexicalDefinition, support recursive analysis

--- src/tokenizer/re-tokenizer.ts
+++ src/tokenizer/re-tokenizer.ts
@@ -1,25 +1,43 @@
 import Token from './token'
+import { tokenizer } from '../Token'
 
 class LexicalDefinition {
-  regex: RegExp
-  captureMap: Map<number, string> = new Map()
-  constructor(def: object) {
-    Object.entries(def).reduce((c, e) => {
-      const [name, regex] = e
-      const groups = LexicalDefinition.getGroups(regex.source)
-      for (let i = c; i <= c + groups; i++) this.captureMap.set(i, name)
-      return c + groups + 1
-    }, 1)
-    this.regex = new RegExp(
-      Object.values(def)
-        .map(r => `(${r.source})`)
-        .join('|'),
-      'g',
-    )
+  tokenizer: Function
+  constructor(d: any) {
+    this.tokenizer = this.parseAction(d.tokenizer)
+  }
+
+  private parseAction(a: any) {
+    if (Array.isArray(a)) {
+      const regex = new RegExp(a.map(({ regex }) => `(${regex.source})`).join('|'), 'g')
+      const captureMap = new Map()
+      a.reduce((g, { regex, action, group }) => {
+        if (action) captureMap.set(g, this.parseAction(action))
+        if (group) group.forEach((gp: any, i: number) => captureMap.set(g + i + 1, this.parseAction(gp)))
+        return g + LexicalDefinition.getGroups(regex) + 1
+      }, 1)
+      return (s: string) => {
+        for (let m = regex.exec(s); m != null; m = regex.exec(s)) {
+          m.forEach((s: any, index: number) => (captureMap.get(index) ? captureMap.get(index)(s) : 0))
+        }
+      }
+    } else if (typeof a === 'object') {
+      const { regex, action, group } = a
+      return (s: any): any => {
+        const m = regex.exec(s)
+        if (action) this.parseAction(action)(m[0])
+        if (group) group.forEach((g: any, index: number) => this.parseAction(g)(m[index + 1]))
+      }
+    } else if (typeof a === 'string') {
+      return (s: any) => {
+        if (s) console.log(`${s} ${a}`)
+      }
+    }
   }
 
   // How many capturing groups in regular expression
-  static getGroups(s: string): number {
+  static getGroups(regex: RegExp): number {
+    const s = regex.source
     let ignore = false,
       pass = false,
       count = 0
@@ -40,10 +58,11 @@ class LexicalDefinition {
 }
 
 const ld = new LexicalDefinition({
-  Identifiers: /([a-zA-Z_\$][\w\$]*)(\s*)(:?)/,
-  Brackets: /[{}()\[\]]/,
-  Delimiter: /[;,.]/,
-  Symbols: /[~!@#%\^&*-+=|\\:`<>.?\/]+/,
+  tokenizer: [
+    { regex: /[;,.]/, action: 'delimiter' },
+    { regex: /([a-zA-Z_\$][\w\$]*)(\s*)(:?)/, group: ['identifier', 'white', 'delimiter'] },
+    { regex: /[{}()\[\]]/, action: 'bracket' },
+  ],
 })
 
 class RETokenizer {
@@ -51,20 +70,7 @@ class RETokenizer {
   tokens: Array<Token> = []
   constructor(text: string) {
     this.text = text
-    this.match()
-    console.log(this.tokens)
-  }
-
-  match() {
-    for (let m = ld.regex.exec(this.text); m != null; m = ld.regex.exec(this.text)) {
-      this.tokens.push(
-        new Token(
-          m[0],
-          { start: { line: 1, column: 0 }, end: { line: 1, column: 0 } },
-          ld.captureMap.get(m.lastIndexOf(m[0])),
-        ),
-      )
-    }
+    ld.tokenizer(text)
   }
 }
 
