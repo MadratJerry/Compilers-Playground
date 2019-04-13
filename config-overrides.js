612f3d3406d5a257d32ae8a3fe8ccb4ad917208f
Yotau Hong <crazymousethief@outlook.com>
2019-04-14 03:27:08 +0800

    🔧 Add jest moduleNameMapper

--- config-overrides.js
+++ config-overrides.js
@@ -2,12 +2,21 @@ const path = require('path')
 const RewireReactHotLoader = require('react-app-rewire-hot-loader')
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
 
-module.exports = function override(config, env) {
-  config = RewireReactHotLoader(config, env)
-  config.plugins.push(new MonacoWebpackPlugin())
-  config.resolve = {
-    ...config.resolve,
-    alias: { '@': path.resolve(__dirname, 'src') },
-  }
-  return config
+module.exports = module.exports = {
+  webpack: function(config, env) {
+    config = RewireReactHotLoader(config, env)
+    config.plugins.push(new MonacoWebpackPlugin())
+    config.resolve = {
+      ...config.resolve,
+      alias: { '@': path.resolve(__dirname, 'src') },
+    }
+    return config
+  },
+  jest: function(config) {
+    config.moduleNameMapper = {
+      ...config.moduleNameMapper,
+      '^@/(.*)$': '<rootDir>/src/$1',
+    }
+    return config
+  },
 }
