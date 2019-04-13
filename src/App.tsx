e62c60c94861370c701de1fa134a92d16cb1ce2d
Yotau Hong <crazymousethief@outlook.com>
2019-04-14 02:36:59 +0800

    🎨 Improve App component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,17 @@
 import { hot } from 'react-hot-loader'
-import React, { useState } from 'react'
-import Editor, { Props as EditorProps } from '@/components/Editor'
-import Monarch from '@/lib/tokenizer'
-import Token from '@/lib/tokenizer/token'
-import simpleC from '@/lib/languages/simpleC'
-import NavagationButton from '@/components/NavagationButton'
+import React from 'react'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { createMuiTheme } from '@material-ui/core/styles'
+import { ThemeProvider } from '@material-ui/styles'
+import Home from './Home'
 
-const tokenizer = new Monarch(simpleC)
-const App = () => {
-  const defaultText = '`number ${n}`/*int this \n/*is*/ a comment\n*/ double pi = 3.1415;'
-  const [toekns, setTokens] = useState<Token[]>(tokenizer.tokenize(defaultText))
+const theme = createMuiTheme({ typography: { useNextVariants: true } })
 
-  const editorRef = React.useRef<NonNullable<EditorProps['editorRef']>['current']>()
-
-  React.useEffect(() => {
-    if (editorRef.current) {
-      const editor = editorRef.current
-      const model = editor.getModel()
-
-      if (model) {
-        model.onDidChangeContent(() => {
-          const tokenList = tokenizer.tokenize(model.getValue())
-          setTokens(tokenList)
-        })
-      }
-    }
-  }, [])
-
-  return (
-    <div>
-      <NavagationButton />
-      <h1>Content</h1>
-      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <Editor
-          editorRef={editorRef}
-          style={{ width: '50%', height: 300 }}
-          options={{
-            value: defaultText,
-            lineNumbers: 'off',
-            minimap: { enabled: false },
-          }}
-        />
-        <ul>
-          {toekns.map((token, index) => (
-            <li key={index}>{token.toString()}</li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  )
-}
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <Home />
+  </ThemeProvider>
+)
 
 export default (process.env.NODE_ENV === 'development' ? hot(module)(App) : App)
---
+++ src/Home.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import Editor, { Props as EditorProps } from '@/components/Editor'
+import Monarch from '@/lib/tokenizer'
+import Token from '@/lib/tokenizer/token'
+import simpleC from '@/lib/languages/simpleC'
+import NavagationButton from '@/components/NavagationButton'
+
+const tokenizer = new Monarch(simpleC)
+
+const Home = () => {
+  const defaultText = '`number ${n}`/*int this \n/*is*/ a comment\n*/ double pi = 3.1415;'
+  const [toekns, setTokens] = useState<Token[]>(tokenizer.tokenize(defaultText))
+
+  const editorRef = React.useRef<NonNullable<EditorProps['editorRef']>['current']>()
+
+  React.useEffect(() => {
+    if (editorRef.current) {
+      const editor = editorRef.current
+      const model = editor.getModel()
+
+      if (model) {
+        model.onDidChangeContent(() => {
+          const tokenList = tokenizer.tokenize(model.getValue())
+          setTokens(tokenList)
+        })
+      }
+    }
+  }, [])
+
+  return (
+    <div>
+      <NavagationButton />
+      <h1>Content</h1>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Editor
+          editorRef={editorRef}
+          style={{ width: '50%', height: 300 }}
+          options={{
+            value: defaultText,
+            lineNumbers: 'off',
+            minimap: { enabled: false },
+          }}
+        />
+        <ul>
+          {toekns.map((token, index) => (
+            <li key={index}>{token.toString()}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Home
