d2ce325effdc24ab65b57c137ba5ed97bc9e60fa
Yotau Hong <crazymousethief@outlook.com>
2019-04-21 16:27:57 +0800

    ✨ Add a LL1 Parser

---
+++ src/lib/parser/LL1Parser.ts
@@ -0,0 +1,74 @@
+import LL1Grammars from '@/lib/grammar/ll1Grammars'
+import * as Grammar from '@/lib/grammar/grammarTypes'
+import Grammars, { epsilon, $accept, $end } from '@/lib/grammar/grammars'
+import { Token } from '@/lib/tokenizer'
+
+type PredictiveTable = Map<Grammar.Symbol, Map<Grammar.Symbol, number>>
+
+export default class LL1Parser {
+  private readonly _predictiveTable: PredictiveTable = new Map()
+  private readonly _grammars: Grammars
+
+  constructor(grammars: LL1Grammars) {
+    this._grammars = grammars
+    grammars.getProductions().forEach((production, i) => {
+      const [symbol, alternative] = production
+      const firstSet = grammars.first(alternative)
+      const add = (s: Grammar.Symbol) => (grammars.terminals.has(s) ? this.addProduction(i, symbol, s) : null)
+      firstSet.forEach(add)
+      if (firstSet.has(epsilon)) grammars.follow(symbol).forEach(add)
+    })
+  }
+
+  public getPredictiveTable(): PredictiveTable {
+    return this._predictiveTable
+  }
+
+  public parse(tokens: Token[]) {
+    const stack = [$accept]
+    let index = 0,
+      X = $accept
+
+    while (X !== $end) {
+      const token = tokens[index]
+
+      if (X === epsilon) stack.pop()
+      else if (this._grammars.terminals.has(X)) {
+        if (token.type === X || token.token === X) {
+          stack.pop()
+          index++
+        } else throw new Error()
+      } else if (this.getM(X, token.type) === undefined && this.getM(X, token.token) === undefined) {
+        throw new Error()
+      } else {
+        const productions = this._grammars.getProductions()
+        const production =
+          this.getM(X, token.type) === undefined
+            ? productions[this.getM(X, token.token)!]
+            : productions[this.getM(X, token.type)!]
+        const [, alternative] = production
+        stack.pop()
+        stack.push(...[...alternative].reverse())
+      }
+
+      X = stack[stack.length - 1]
+    }
+  }
+
+  private getM(nonTerminal: Grammar.Symbol, terminal: Grammar.Symbol): number | undefined {
+    const tMap = this._predictiveTable.get(nonTerminal)
+    if (tMap) {
+      return tMap.get(terminal)
+    } else return undefined
+  }
+
+  private addProduction(index: number, nonTerminal: Grammar.Symbol, terminal: Grammar.Symbol) {
+    let tMap = this._predictiveTable.get(nonTerminal)
+    if (!tMap) {
+      tMap = new Map()
+      this._predictiveTable.set(nonTerminal, tMap)
+    }
+    if (tMap.has(terminal)) throw new Error(`M[${nonTerminal}, ${terminal}] can only be one item`)
+    tMap.set(terminal, index)
+  }
+}
