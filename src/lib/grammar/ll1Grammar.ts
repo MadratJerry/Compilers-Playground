158848dbf6d45c0410a584f9507e31fc9e682014
Yotau Hong <crazymousethief@outlook.com>
2019-04-22 20:55:43 +0800

    🐛 Fix LL1 Grammar check bug and update error info

--- src/lib/grammar/ll1Grammar.ts
+++ src/lib/grammar/ll1Grammar.ts
@@ -1,4 +1,15 @@
-import { Terminal, NonTerminal, epsilon, Alternative, Firsts, Follows, Productions, Symbol, symbolSet } from '.'
+import {
+  Terminal,
+  NonTerminal,
+  epsilon,
+  Alternative,
+  Firsts,
+  Follows,
+  Productions,
+  Symbol,
+  symbolSet,
+  Production,
+} from '.'
 import { Grammar } from './grammar'
 
 export class LeftRecursionError extends Error {}
@@ -42,19 +53,26 @@ export class LL1Grammar extends Grammar {
       if (loc) {
         const [start, end] = loc
         for (let i = start; i < end - 1; i++)
-          for (let j = start + 1; j < end; j++) {
-            const [[, a], [, b]] = [this._productions[i], this._productions[j]]
+          for (let j = i + 1; j < end; j++) {
+            const [pa, pb] = [this._productions[i], this._productions[j]]
+            const [[, a], [, b]] = [pa, pb]
             if (intersection(this.first(a), this.first(b)).size > 0)
               throw new CommonPrefixError(
-                `Grammars have common prefix, because FIRST(${a}) and FIRST(${b}) are not disjoint sets`,
+                `Grammars have common prefix, because FIRST(${this.proudctionToString(
+                  pa,
+                )}) and FIRST(${this.proudctionToString(pb)}) are not disjoint sets`,
               )
             if (this.first(a).has(epsilon) && intersection(this.first(b), this.follow(s)).size > 0)
               throw new DanglingElseError(
-                `Grammars have dangling else, because FIRST(${b}) and FOLLOW(${s}) are not disjoint sets`,
+                `Grammars have dangling else, because FIRST(${this.proudctionToString(
+                  pb,
+                )}) and FOLLOW(${s}) are not disjoint sets`,
               )
             if (this.first(b).has(epsilon) && intersection(this.first(a), this.follow(s)).size > 0)
               throw new DanglingElseError(
-                `Grammars have dangling else, because FIRST(${a}) and FOLLOW(${s}) are not disjoint sets`,
+                `Grammars have dangling else, because FIRST(${this.proudctionToString(
+                  pa,
+                )}) and FOLLOW(${s}) are not disjoint sets`,
               )
           }
       }
@@ -115,5 +133,9 @@ export class LL1Grammar extends Grammar {
     }
 
     return newSet
+  }
+
+  private proudctionToString(p: Production): string {
+    return `${p[0]} -> ${p[1].join(' ')}`
   }
 }
