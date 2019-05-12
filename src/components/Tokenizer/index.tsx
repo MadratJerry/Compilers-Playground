2ef7375d35af3576853f11d2ca23b16bddf1527c
Yotau Hong <crazymousethief@outlook.com>
2019-05-12 22:13:37 +0800

    ✨ Add tokens hover decoration

--- src/components/Tokenizer/index.tsx
+++ src/components/Tokenizer/index.tsx
@@ -1,10 +1,11 @@
 import React, { useRef, useEffect, useState, useCallback } from 'react'
-import MonacoEditor, { ChangeHandler } from 'react-monaco-editor'
+import MonacoEditor from 'react-monaco-editor'
 import * as monaco from 'monaco-editor'
 import { makeStyles, createStyles } from '@material-ui/styles'
-import Typography from '@material-ui/core/Typography'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import Typography from '@material-ui/core/Typography'
 import Paper from '@material-ui/core/Paper'
 import { Monarch, Token } from '@/lib/tokenizer'
 import './index.css'
@@ -53,6 +54,16 @@ const useStyles = makeStyles(() =>
     container: {
       display: 'flex',
       justifyContent: 'space-between',
+      '& > div:nth-child(1)': {
+        width: '60%',
+      },
+      '& > div:nth-child(2)': {
+        width: '40%',
+      },
+    },
+    tokenList: {
+      overflow: 'scroll',
+      height: 450,
     },
   }),
 )
@@ -60,13 +71,13 @@ const useStyles = makeStyles(() =>
 const posStack = (value: string) => {
   const stack: Array<number> = [0]
   for (let i = 0; i < value.length; i++) if (value.charAt(i) === '\n') stack.push(i + 1)
-  stack.push(value.length)
+  stack.push(value.length + 1)
   return stack
 }
 
 const pos = (offset: number, stack: Array<number>) => {
   for (let i = 0; i < stack.length - 1; i++) {
-    if (offset >= stack[i] && offset <= stack[i + 1]) {
+    if (offset >= stack[i] && offset < stack[i + 1]) {
       return [i, offset - stack[i]]
     }
   }
@@ -79,12 +90,30 @@ const Tokenizer = () => {
   const classes = useStyles()
   const [tokens, setTokens] = useState<RangeToken[]>([])
   const [language, setLanguage] = useState(defaultText)
-  const [sample, setSample] = useState(`"it's a string"\n/*it's a \ncomment*/\nbool a `)
+  //   const [sample, setSample] = useState(`"it's a string"\n/*it's a \ncomment*/\nbool a `)
+  const [sample, setSample] = useState(`a = 1`)
   const [dispose, setDispose] = useState<monaco.IDisposable | null>(null)
   const languageEditor = useRef<monaco.editor.ICodeEditor>({} as monaco.editor.ICodeEditor)
   const sampleEditor = useRef<monaco.editor.ICodeEditor>({} as monaco.editor.ICodeEditor)
+  const hoverDecoration = useRef<Array<string>>([])
+  const tokenDecoration = useRef<Array<string>>([])
 
-  const handleResize = () => sampleEditor.current!.layout()
+  const handleMouseOver = useCallback(
+    range => () => {
+      hoverDecoration.current = sampleEditor.current.deltaDecorations(hoverDecoration.current, [
+        {
+          range: range,
+          options: {
+            inlineClassName: 'inlineTokenHighlight',
+          },
+        },
+      ])
+    },
+    [tokens],
+  )
+
+  const handleMouseOut = () =>
+    (hoverDecoration.current = sampleEditor.current.deltaDecorations(hoverDecoration.current, []))
 
   useEffect(() => {
     if (dispose) dispose.dispose()
@@ -95,14 +124,21 @@ const Tokenizer = () => {
       const tokenizer = new Monarch(config)
       rangeTokens = tokenizer.tokenize(sample) as RangeToken[]
       const stack = posStack(sample)
+      sampleEditor.current.deltaDecorations(tokenDecoration.current, [])
       for (const token of rangeTokens) {
         const [startLineNumber, startColumn] = pos(token.offset, stack)
-        const [endLineNumber, endColumn] = pos(token.offset + token.token.length - 1, stack)
-        token.range = new monaco.Range(startLineNumber + 1, startColumn + 1, endLineNumber + 1, endColumn + 2)
-        sampleEditor.current.deltaDecorations(
+        const [endLineNumber, endColumn] = pos(token.offset + token.token.length, stack)
+        token.range = new monaco.Range(startLineNumber + 1, startColumn + 1, endLineNumber + 1, endColumn + 1)
+        const [decoration] = sampleEditor.current.deltaDecorations(
           [],
-          [{ range: token.range, options: { inlineClassName: 'tokenDecoration' } }],
+          [
+            {
+              range: token.range,
+              options: { inlineClassName: 'tokenDecoration' },
+            },
+          ],
         )
+        tokenDecoration.current.push(decoration)
       }
 
       setTokens(rangeTokens)
@@ -120,7 +156,7 @@ const Tokenizer = () => {
                 { value: `offset: ${token.offset} range:${token.range.toString()}` },
               ],
             }
-          : { range: new monaco.Range(1, 1, 1, 1), contents: [] }
+          : { range: new monaco.Range(0, 0, 0, 0), contents: [] }
       },
     })
     setDispose(d)
@@ -128,6 +164,10 @@ const Tokenizer = () => {
   }, [language, sample])
 
   useEffect(() => {
+    const handleResize = () => {
+      languageEditor.current.layout()
+      sampleEditor.current.layout()
+    }
     window.addEventListener('resize', handleResize)
     return () => window.removeEventListener('resize', handleResize)
   }, [])
@@ -135,31 +175,51 @@ const Tokenizer = () => {
   return (
     <>
       <Typography variant="h2" gutterBottom>
-        Tokenizer
+        Definition
       </Typography>
-      <Paper className={classes.container}>
+      <Paper>
         <MonacoEditor
-          width="50%"
-          height="500"
+          height="450"
           language="javascript"
-          options={{
-            minimap: { enabled: false },
-          }}
           onChange={useCallback(v => setLanguage(v), [])}
           editorDidMount={useCallback(e => (languageEditor.current = e), [])}
           value={language}
         />
-        <MonacoEditor
-          width="50%"
-          height="500"
-          language="sampleLanguage"
-          options={{
-            minimap: { enabled: false },
-          }}
-          onChange={useCallback(v => setSample(v), [])}
-          editorDidMount={useCallback(e => (sampleEditor.current = e), [])}
-          value={sample}
-        />
+      </Paper>
+      <Typography variant="h2" gutterBottom>
+        Result
+      </Typography>
+      <Paper className={classes.container}>
+        <div>
+          <Typography variant="subtitle1" gutterBottom>
+            Language Editor
+          </Typography>
+          <MonacoEditor
+            width="100%"
+            height="450"
+            language="sampleLanguage"
+            options={{
+              renderControlCharacters: true,
+              minimap: { enabled: false },
+            }}
+            onChange={useCallback(v => setSample(v), [])}
+            editorDidMount={useCallback(e => (sampleEditor.current = e), [])}
+            value={sample}
+          />
+        </div>
+        <div>
+          <Typography variant="subtitle1" gutterBottom>
+            Tokens
+          </Typography>
+          <List component="ul" onMouseOutCapture={handleMouseOut} className={classes.tokenList}>
+            {tokens.map(({ offset, type, token, range }) => (
+              <ListItem key={offset} button>
+                <ListItemText primary={type} onMouseOverCapture={handleMouseOver(range)} />
+                {range.toString()}
+              </ListItem>
+            ))}
+          </List>
+        </div>
       </Paper>
     </>
   )
